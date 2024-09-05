0c7e57ff838cadbbb8e4a86bda926ae98fcacee4
GARIB-CODERS <landrepo541@gmail.com>
2024-09-05 17:30:59 +0530

    Update play.py

--- Spotify_Music/utils/inline/play.py
+++ Spotify_Music/utils/inline/play.py
@@ -67,7 +67,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
        [
-            InlineKeyboardButton(text="⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂⎯꯭ ꯭̽🌸", url="https://t.me/ll_BAD_MUNDA_ll"),
+            InlineKeyboardButton(text="Ⓚ︎Ⓘ︎Ⓝ︎Ⓖ︎", url="https://t.me/ll_ISTKHAR_ll"),
         InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -83,7 +83,7 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
        [
-            InlineKeyboardButton(text="⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂⎯꯭ ꯭̽🌸", url="https://t.me/ll_BAD_MUNDA_ll"),
+            InlineKeyboardButton(text="Ⓚ︎Ⓘ︎Ⓝ︎Ⓖ︎", url="https://t.me/ll_ISTKHAR_ll"),
         InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
